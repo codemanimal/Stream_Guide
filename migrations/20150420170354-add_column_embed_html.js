e0dab4508da40b7b32f049ed8b6cda39df8b7b96
Mark Smukler <smukler.mark@gmail.com>
2015-04-20 17:13:55 -0400

    migrated to development db

--- migrations/20150420170354-add_column_embed_html.js
+++ migrations/20150420170354-add_column_embed_html.js
@@ -3,7 +3,6 @@ module.exports = {
   up: function(migration, DataTypes, done) {
     migration.addColumn('videos', 'embed_html', {
     	type: DataTypes.TEXT,
-    	allowNull: false
     }).done(done);
   },
 
